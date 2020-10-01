d4808fe7df6a28b34fce81c844c111b0c66f3377
Zach <z61@zeesixtyone.com>
2020-10-01 17:42:44 -0400

    Make command more dynamic

--- commands/misc/howlongtobeat.js
+++ commands/misc/howlongtobeat.js
@@ -1,61 +1,52 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
 const commando = require("discord.js-commando");
-const hltb = require('howlongtobeat');
+const hltb = require("howlongtobeat");
 
 module.exports = class HowLongToBeatCommand extends commando.Command {
   constructor(client) {
     super(client, {
       name: "howlongtobeat",
       group: "misc",
-      aliases: ['hltb'],
+      aliases: ["hltb"],
       memberName: "howlongtobeat",
       description: "Searches how long to beat for game times",
       args: [
         {
-          key: 'game',
-          prompt: 'What game to search',
-          type: 'string'
-        }
-      ]
+          key: "game",
+          prompt: "What game to search",
+          type: "string",
+        },
+      ],
     });
   }
 
-  async run(message, {game}) {
+  async run(message, { game }) {
     const hltbService = new hltb.HowLongToBeatService();
 
-    const [result] = await hltbService.search(game)
+    const [result] = await hltbService.search(game);
 
     if (!result) {
-      await message.reply(`Unable to find game`)
-      return
+      await message.reply(`Unable to find game`);
+      return;
     }
 
-    const [main, extra, completionist] = result.timeLabels
-    let embedData = {
-      "title": result.name,
-      "color": 53380,
-      "thumbnail": {
-        url: result.imageUrl
+    const labels = result.timeLabels;
+
+    const embedData = {
+      title: result.name,
+      color: 53380,
+      thumbnail: {
+        url: result.imageUrl,
       },
-      "fields": [
-        {
-          name: main[1],
-          value: `${result.gameplayMain.toString()} hours`,
-          inline: true
-        },
-        {
-          name: extra[1],
-          value: `${result.gameplayMainExtra.toString()} hours`,
-          inline: true
-        },
-        {
-          name: completionist[1],
-          value: `${result.gameplayCompletionist.toString()} hours`,
-          inline: true
-        }
-      ]
+    };
+
+    const embed = new Discord.RichEmbed(embedData);
+    console.log(labels);
+    for (const [key, phrase] of labels) {
+      if (result[key] > 0) {
+        embed.addField(phrase, `${result[key]} hours`);
+      }
     }
-    const embed = new Discord.RichEmbed(embedData)
-    await message.channel.send(embed)
+    await message.channel.send(embed);
   }
 };
