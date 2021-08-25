855a7be4749c6d244f20bfd698fbff9135cd0d23
Eric Hardiman <hardimaneric@gmail.com>
2021-08-25 17:29:19 +0000

    Not done yet lol

--- src/Helpers/Spotify/GetCurrentPlayingTrack.ts
+++ src/Helpers/Spotify/GetCurrentPlayingTrack.ts
@@ -1,17 +1,19 @@
-import axios from 'axios';
-import { ISpotifyLink } from '../../Database/SpotifyLink';
-import { SpotifyTrack } from '../../global';
+import axios from "axios";
+import { ISpotifyLink } from "../../Database/SpotifyLink";
+import { SpotifyTrack } from "../../global";
 
 const GetCurrentPlayingTrack = async (spotifyLink: ISpotifyLink) => {
   const { access_token } = spotifyLink;
 
-  const res = await axios.request({
-    method: 'GET',
-    url: 'https://api.spotify.com/v1/me/player/currently-playing',
-    headers: { Authorization: access_token },
+  const {
+    data: { item },
+  } = await axios.request({
+    method: "GET",
+    url: "https://api.spotify.com/v1/me/player/currently-playing",
+    headers: { Authorization: `Bearer ${access_token}` },
   });
 
-  const { name, artists } = res.data as SpotifyTrack;
+  const { name, artists } = item as SpotifyTrack;
 
   return `${name} ${artists[0].name}`;
 };
